407e6e66e7c710add9117688d78e81e390da87be
Chris Ziogas <ziogaschr@gmail.com>
2020-03-17 15:01:37 +0200

    Add base ENS contract

--- ens/migrations/2_deploy_contracts.js
+++ ens/migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 const ENS = artifacts.require('ENS');
 
-const ensContractJson = require('../build/contracts/ENS.json.js');
+const ensContractJson = require('../build/contracts/ENS.json');
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(ENS);
 
   console.log('ENS address: ', ENS.address);
-  console.warn('----------------');
+  console.log('----------------');
 
   const instance = await ENS.deployed();
 
@@ -45,4 +45,67 @@ module.exports = async function(deployer, network, accounts) {
 
   const gas = await cmd.estimateGas();
   await cmd.send({ from: accounts[0], gas: gas });
+
+  // Test use for development
+  if (network === 'development') {
+
+    console.log('\n----------------\nDeployment tests\n----------------\n');
+
+    const testLabel =
+      '0xde9b09fd7c5f901e23a3f19fecc54828e9c848539801e86591bd9801b019f84f';
+    const testBuyer = accounts[0];
+    const testOwner1 = '0x8F10D3A6283672EcfAeea0377d460BdEd489EC44';
+    const testOwner2 = '0x6FDFD8Bf1A5310243519dC2e7B90916f6b4534ab';
+
+    try {
+      const registrationAmount = await instance.getRegistrationAmount();
+      console.log('RegistrationAmount: %d', registrationAmount)
+
+      const receipt = await instance.register(testLabel, testOwner1, {
+        from: testBuyer,
+        value: registrationAmount,
+        // value: web3.utils.toWei('0.2', 'ether'), // test excess amount
+      });
+      console.info(
+        'Label got inserted: %s, %s',
+        receipt.logs[0].args.label,
+        receipt.logs[0].args.owner
+      );
+    } catch (err) {
+      console.error('Label insertion err: ', err.message);
+    }
+
+    try {
+      const owner = await instance.owner(testLabel);
+
+      if (owner !== testOwner1) {
+        console.error(
+          "Owner retrieved doesn't match with the owner registered"
+        );
+      }
+
+      console.info('Owner: %s', owner);
+    } catch (err) {
+      console.error('Get owner err: ', err.message);
+    }
+
+    try {
+      const expiresAt = await instance.expiresAt(testLabel);
+
+      console.info('ExpiresAt: %d', expiresAt);
+    } catch (err) {
+      console.error('Get expiresAt err: ', err.message);
+    }
+
+    try {
+      const receipt = await instance.transfer(testLabel, testOwner2);
+      console.info(
+        'Label got transfered: %s, %s',
+        receipt.logs[0].args.label,
+        receipt.logs[0].args.newOwner
+      );
+    } catch (err) {
+      console.error('Label transfer err: ', err.message);
+    }
+  }
 };
